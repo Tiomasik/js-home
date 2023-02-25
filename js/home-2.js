7396a571e0ed377999dae683f695458b99c8fea9
Artem Maslii <a.masliy@ukr.net>
2023-02-25 15:24:22 +0200

    Update home-2.js

--- js/home-2.js
+++ js/home-2.js
@@ -1,23 +1,45 @@
-const students = [
-  { name: "Манго", score: 83 },
-  { name: "Полі", score: 59 },
-  { name: "Аякс", score: 37 },
-  { name: "Ківі", score: 94 },
-];
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Полі", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Ківі", score: 94 },
+// ];
 
-const inAscendingScoreOrder = students.sort(
-  (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
-);
-console.log(inAscendingScoreOrder)
+// const inAscendingScoreOrder = students.sort(
+//   (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+// );
+// console.log(inAscendingScoreOrder)
 
-const inDescendingScoreOrder = students.sort(
-  (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
-);
+// const inDescendingScoreOrder = students.sort(
+//   (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
+// );
 
-console.log(inDescendingScoreOrder)
+// console.log(inDescendingScoreOrder)
 
-const inAlphabeticalOrder = students.sort((firstStudent, secondStudent) =>
-  firstStudent.name.localeCompare(secondStudent.name)
-);
+// const inAlphabeticalOrder = students.sort((firstStudent, secondStudent) =>
+//   firstStudent.name.localeCompare(secondStudent.name)
+// );
 
-console.log(inAlphabeticalOrder)
+// console.log(inAlphabeticalOrder)
+
+const user = {
+  name: "Jacques Gluke",
+  tag: "jgluke",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const {
+  name,
+  tag,
+  stats: { followers, views: userViews, likes: userLikes = 0 },
+} = user;
+
+console.log(name); // Jacques Gluke
+console.log(tag); // jgluke
+console.log(followers); // 5603
+console.log(userViews); // 4827
+console.log(userLikes); // 1308
